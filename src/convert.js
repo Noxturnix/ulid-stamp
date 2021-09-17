f34a4a7fd7ebdf824c992177fee8b5e35417d26d
Noxturnix <noxturnix@noxt.us>
2021-09-17 00:28:57 -0700

    convertable ULID

--- src/convert.js
+++ src/convert.js
@@ -1,21 +1,43 @@
-// Converts a snowflake ID string into a JS Date object using the provided epoch (in ms), or Discord's epoch if not provided
-export function convertSnowflakeToDate(snowflake, epoch = DISCORD_EPOCH) {
-	return new Date(snowflake / 4194304 + epoch)
+/*
+ * This file contains implemented codes which are referenced
+ * from ulid/javascript by Alizain Feerasta.
+ *
+ * ulid/javascript is released under MIT License
+ * https://github.com/ulid/javascript/blob/master/LICENSE
+ */
+
+export const ULID_LETTERS = '0123456789ABCDEFGHJKMNPQRSTVWXYZ'
+export const ULID_LETTERS_LENGTH = ULID_LETTERS.length
+export const ULID_TIME_MAX = 2 ** 48 - 1
+
+// Converts a snowflake ID string into a JS Date object
+export function convertSnowflakeToDate(id) {
+	return new Date(
+		id
+			.substr(0, 10)
+			.split('')
+			.reverse()
+			.reduce((p, c, i) => {
+				return (p += ULID_LETTERS.indexOf(c) * ULID_LETTERS_LENGTH ** i)
+			}, 0)
+	)
 }
 
-export const DISCORD_EPOCH = 1420070400000
-
 // Validates a snowflake ID string and returns a JS Date object if valid
-export function validateSnowflake(snowflake, epoch) {
-	if (!Number.isInteger(+snowflake)) {
-		throw "That doesn't look like a snowflake. Snowflakes contain only numbers."
+export function validateSnowflake(id) {
+	if (id.length !== 26) {
+		throw "That doesn't look like an ID. IDs have length of 26."
 	}
-	if (snowflake < 4194304) {
-		throw "That doesn't look like a snowflake. Snowflakes are much larger numbers."
-	}
-	const timestamp = convertSnowflakeToDate(snowflake, epoch)
+	id.split('').map((l) => {
+		if (ULID_LETTERS.indexOf(l) === -1)
+			throw "That doesn't look like an ID. IDs may contain certain characters."
+	})
+	const timestamp = convertSnowflakeToDate(id)
 	if (isNaN(timestamp.getTime())) {
-		throw "That doesn't look like a snowflake. Snowflakes have fewer digits."
+		throw "That doesn't look like an ID. IDs have fewer digits."
+	}
+	if (timestamp.valueOf() > ULID_TIME_MAX) {
+		throw "That doesn't look like an ID. IDs can't be this large."
 	}
 	return timestamp
 }
